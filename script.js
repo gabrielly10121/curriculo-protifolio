b63bb0ab45693a4b7432a44e41ca586402992083
Gabrielly10121 <gabriellymoura.moura12@gmail.com>
2024-08-22 19:45:26 -0300

    Projeto finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var typed = new typed(".text",{
+    String:("Desenvolvedora Front-end", "Analista de Dados", "Web Designer"),
+    typeSpeed: 100,
+    backSpeed:100,
+    BackDelay: 1000,
+    loop: true
+})
